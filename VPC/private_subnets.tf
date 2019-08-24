39f5e481608129215ef0319d6ee7342c4a083c92
nextinstall <nextinstall@next.install>
2019-08-23 21:32:38 -0400

    vpc inital build

---
+++ VPC/private_subnets.tf
@@ -0,0 +1,29 @@
+##############################
+#PRIVATE SUBNETS
+##############################
+resource "aws_subnet" "private" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  count             =  "${length(var.private_subnets)}"
+  cidr_block        = "${var.private_subnets[count.index]}"
+  availability_zone = "${var.azs[count.index]}"
+  tags     = { Name = "${var.Name}-${var.azs[count.index]}-private"}
+  lifecycle { create_before_destroy = true }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  count   = "${length(var.private_subnets)}"
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.nat.*.id[count.index]}"
+  }
+
+  tags   = { Name = "${var.Name}.priv_route_table.${var.azs[count.index]}" }
+  lifecycle { create_before_destroy = true }
+}
+
+resource "aws_route_table_association" "private" {
+  count              = "${length(var.private_subnets)}"
+  subnet_id          = "${aws_subnet.private.*.id[count.index]}"
+  route_table_id     = "${aws_route_table.private.*.id[count.index]}"
+}
